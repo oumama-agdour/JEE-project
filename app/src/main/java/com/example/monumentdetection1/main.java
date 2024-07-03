7ffd2747f1a1112f1d1b5619ed0ed57e9ad98342
oumama <oumamaagdour15@gmail.com>
2024-07-03 23:57:21 +0100

    third commit

--- app/src/main/java/com/example/monumentdetection1/main.java
+++ app/src/main/java/com/example/monumentdetection1/main.java
@@ -2,26 +2,45 @@ package com.example.monumentdetection1;
 import com.example.monumentdetection1.R.id;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Intent;
 
 import android.os.Bundle;
 
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class main extends AppCompatActivity {
     MaterialToolbar topAppBar;
+    private MonumentAdapter monumentAdapter;
+    private List<Monument> monumentList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private FirebaseFirestore db;
 
     @SuppressLint("NonConstantResourceId")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         topAppBar = findViewById(id.topAppBar);
+        monumentAdapter = new MonumentAdapter(monumentList, this);
+
+        recyclerView.setAdapter(monumentAdapter);
+        FirebaseApp.initializeApp(this);
+        db = FirebaseFirestore.getInstance();
+
+        loadMonuments();
 
         // Gestion des clics sur les éléments de menu
 
@@ -31,10 +50,32 @@ public class main extends AppCompatActivity {
             }
             return false;
         });
+        topAppBar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == id.search){
+                startActivity(new Intent(main.this, MonumentActivity.class));
+            }
+            return false;
+        });
 
 
     }
-
-
+    private void loadMonuments() {
+        db.collection("monuments")
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Monument monument = document.toObject(Monument.class);
+                            monumentList.add(monument);
+                        }
+                        monumentAdapter.notifyDataSetChanged();
+                    } else {
+                        Toast.makeText(main.this, "Failed to load data", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(main.this, "Failed to load data", Toast.LENGTH_SHORT).show();
+                });
+    }
 
 }
